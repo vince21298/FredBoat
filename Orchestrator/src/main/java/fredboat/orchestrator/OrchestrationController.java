66c42215125e4470ec40b72f8222446a68227170
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-04-26 19:43:06 +0200

    Actually use packages

--- Orchestrator/src/main/java/fredboat/orchestrator/OrchestrationController.java
+++ Orchestrator/src/main/java/fredboat/orchestrator/OrchestrationController.java
@@ -23,6 +23,8 @@
  *
  */
 
+package fredboat.orchestrator;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
