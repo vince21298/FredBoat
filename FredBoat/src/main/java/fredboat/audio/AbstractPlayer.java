8d6b155ed037340403fb950c0868c700015e81d7
Napster <napster@npstr.space>
2017-07-16 00:26:04 +0200

    Moar audio loading threads

--- FredBoat/src/main/java/fredboat/audio/AbstractPlayer.java
+++ FredBoat/src/main/java/fredboat/audio/AbstractPlayer.java
@@ -93,7 +93,7 @@ public abstract class AbstractPlayer extends AudioEventAdapter implements AudioS
                 playerManager.useRemoteNodes(Config.CONFIG.getLavaplayerNodes());
             }
             
-            playerManager.setItemLoaderThreadPoolSize(100);
+            playerManager.setItemLoaderThreadPoolSize(500);
         }
     }
 
